9f7ca6b30601bfa5f030f8c77249ea15eae5039e
Ajith <ajith.vypin@gmail.com>
2019-04-25 23:47:44 -0700

    KVO Implemented

--- KVO/KVO/ViewController.swift
+++ KVO/KVO/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class User: NSObject {
+    @objc dynamic var age = 10
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var lblNewValue: UILabel!
+    @IBOutlet weak var txtNewValue: UITextField!
+    @IBOutlet weak var btnUpdate: UIButton!
+    
+    @objc let user = User()
+    var ageObservationToken: NSKeyValueObservation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        ageObservationToken = observe(\ViewController.user.age, options: [.new]) { (self, value) in
+            let text = value.newValue ?? 0
+            self.lblNewValue.text = String(text)
+        }
     }
 
+    @IBAction func updateLabel(sender: UIButton) {
+        guard let userInput = txtNewValue.text else { return }
+        user.age = Int(userInput) ?? 0
+    }
 
 }
 
