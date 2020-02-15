ebd43b60203c0d6fd864ac4afa4ff8034d8aace2
Ajith <ajith.vypin@gmail.com>
2020-02-15 13:46:31 -0800

    initial commit

---
+++ Coding_Interview/Facebook/Challange/NobelLaureates/NobelLaureates/LocationSearchTableViewController.swift
@@ -0,0 +1,97 @@
+//
+//  LocationSearchTableViewController.swift
+//  NobelLaureates
+//
+//  Created by Ajith Anthony on 2/15/20.
+//  Copyright © 2020 Ajith Antony. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+protocol HandleMapSearch: class {
+    func dropPinZoomIn(placemark: MKPlacemark)
+}
+
+class LocationSearchTableViewController: UITableViewController {
+    var matchingItems: [MKMapItem] = []
+    var mapView: MKMapView? = nil
+    weak var handleMapSearchDelegate: HandleMapSearch?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Methods
+    func getAddress(from selectedItem:MKPlacemark) -> String {
+        // put a space between "4" and "Melrose Place"
+        let firstSpace = (selectedItem.subThoroughfare != nil &&
+                            selectedItem.thoroughfare != nil) ? " " : ""
+        
+        // put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) &&
+                    (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        
+        // put a space between "Washington" and "DC"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil &&
+                            selectedItem.administrativeArea != nil) ? " " : ""
+        
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            selectedItem.thoroughfare ?? "",
+            comma,
+            selectedItem.locality ?? "",
+            secondSpace,
+            selectedItem.administrativeArea ?? ""
+        )
+        
+        return addressLine
+    }
+}
+
+// MARK: Table view delegates and data source
+extension LocationSearchTableViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return matchingItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        let selectedItem = matchingItems[indexPath.row].placemark
+        cell.textLabel?.text = selectedItem.name
+        cell.detailTextLabel?.text = getAddress(from: selectedItem)
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = matchingItems[indexPath.row].placemark
+        handleMapSearchDelegate?.dropPinZoomIn(placemark: selectedItem)
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK:
+extension LocationSearchTableViewController : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let mapView = mapView,
+            let searchBarText = searchController.searchBar.text else { return }
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = searchBarText
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        search.start { response, _ in
+            guard let response = response else {
+                return
+            }
+            self.matchingItems = response.mapItems
+            self.tableView.reloadData()
+        }
+    }
+}
---
+++ Coding_Interview/Facebook/Challange/NobelLaureates/NobelLaureates/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  NobelLaureates
+//
+//  Created by Ajith Anthony on 2/15/20.
+//  Copyright © 2020 Ajith Antony. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController : UIViewController {
+    private let locationManager = CLLocationManager()
+    private var selectedPin: MKPlacemark?
+    private var resultSearchController:UISearchController!
+    private var annotation:MKAnnotation!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpLocationManager()
+        setUpLocationResultView()
+    }
+    
+    // MARK: Local methods
+    private func setUpLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+    }
+    
+    private func setUpLocationResultView() {
+        let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTableViewController
+        locationSearchTable.mapView = mapView
+        locationSearchTable.handleMapSearchDelegate = self
+        resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+        resultSearchController?.searchResultsUpdater = locationSearchTable
+    }
+    
+    @IBAction func showSearchBar(_ sender: AnyObject) {
+        resultSearchController.hidesNavigationBarDuringPresentation = false
+        resultSearchController.searchBar.delegate = self as? UISearchBarDelegate
+        resultSearchController.searchBar.placeholder = "Search for Nobel Laureates"
+        present(resultSearchController, animated: true, completion: nil)
+    }
+}
+
+extension ViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last{
+            let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("error:: (error)")
+    }   
+}
+
+extension ViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark: MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+                annotation.subtitle = "\(city) \(state)"
+        }
+        
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: placemark.coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+}
